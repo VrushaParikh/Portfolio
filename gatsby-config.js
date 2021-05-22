3baa1faf7373f615067959eb2e774c434b5a0d49
Vrusha Parikh <vrushaparikh18@gmail.com>
2021-05-22 11:02:49 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,7 @@ module.exports = {
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
         display: 'minimal-ui',
+        icon: 'src/images/r_logo.png',
        
       },
     },
